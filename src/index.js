2a0794f153bc423241aba47480129c15342b454e
Mateusz Markowski <mateusz94markow@gmail.com>
2023-03-05 14:04:09 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,65 +1,53 @@
-const sum = require('./sum').sum;
-import { sum2 } from './sum2';
 import { style } from './css/index.scss';
-import Icon from './assets/img/proba.png';
 
-console.log('Hello World');
-console.log(sum(2, 3));
-console.log(sum2(2, 3));
-
-let heading = document.querySelector('#demo'),
-	sumValue = sum(10, 5);
-
-heading.innerText = `10+10= ${sumValue}`;
-
-let myIcon = new Image();
-myIcon.src = Icon;
-
-document.querySelector('div').append(myIcon);
-document.querySelector('div').classList.add('change');
+const destinationHTML = document.getElementById('desctination');
+const departureHTML = document.getElementById('departure');
 
 fetch(
 	'https://raw.githubusercontent.com/mmarkowski94/look-up/main/src/endpointy/db.json'
 )
 	.then((response) => response.json())
 	.then((data) => {
-		console.log(data);
+		data.destination.forEach((destinationElem) => {
+			const elem = document.createElement('option');
+			elem.textContent = destinationElem.desc;
+			elem.setAttribute('value', destinationElem.value);
+			destinationHTML.appendChild(elem);
+		}),
+			data.departure.forEach((departureElem) => {
+				const elem = document.createElement('option');
+				elem.textContent = departureElem.desc;
+				elem.setAttribute('value', departureElem.value);
+				departureHTML.appendChild(elem);
+			});
 	})
 	.catch((error) => {
 		console.error(error);
 	});
 
-    const destinationHTML = document.getElementById("desctination");
-    const departureHTML = document.getElementById("departure");
 
-    fetch(
-        'https://raw.githubusercontent.com/mmarkowski94/look-up/main/src/endpointy/db.json'
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            data.destination.forEach(destinationElem => {
-                const elem = document.createElement('option');
-                elem.textContent = destinationElem.desc;
-                elem.setAttribute('value', destinationElem.value);
-                destinationHTML.appendChild(elem);
-            });
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-        fetch(
-            'https://raw.githubusercontent.com/mmarkowski94/look-up/main/src/endpointy/db.json'
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                data.departure.forEach(departureElem => {
-                    const elem = document.createElement('option');
-                    elem.textContent = departureElem.desc;
-                    elem.setAttribute('value', departureElem.value);
-                    departureHTML.appendChild(elem);
-                });
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    // <option value="volvo">Volvo</option>
+
+function hidden() {
+    Array.from(document.getElementById('desctination').children).forEach((children) => {
+		if (children.classList.contains('hidden')) {
+			children.classList.remove('hidden');
+		}
+	});
+
+	const departureChecked = document.getElementById('departure').value;
+    console.log('------')
+    console.log(departureChecked);
+    console.log(document.getElementById('desctination').children);
+	Array.from(document.getElementById('desctination').children).forEach((children) => {
+		if (children.value === departureChecked) {
+			children.classList.add('hidden');
+		}
+	});
+
+    if(document.getElementById('departure').value === document.getElementById('desctination').value) {
+        document.getElementById('desctination').value = "";
+    }
+
+}
+
+departureHTML.addEventListener("change", hidden);
